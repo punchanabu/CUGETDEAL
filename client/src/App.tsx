c54f1a90c23e1af7dff3e54be999206b46d066b5
punpunkeshin05 <97283645+punpunkeshin05@users.noreply.github.com>
2023-10-24 04:49:43 +0700

    init

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+  import React from 'react';
+  import './App.css';
+
+  function App() {
+    return (
+      <div className="App flex justify-center align-center">
+        <p className='text-2xl font-mono underline'>Kuy</p>
+      </div>
+    );
+  }
+
+  export default App;
