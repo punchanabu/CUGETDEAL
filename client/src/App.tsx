c746401789660fcc38557e5b7cd274c5beae308b
MoosaTae <sadhis.tae@gmail.com>
2023-10-25 16:55:39 +0700

    add router path

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,27 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import Register from '../src/components/Register';
+import { createBrowserRouter, Link, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Navbar />,
+    children: [
+      {
+        path: 'register',
+        element: <Register />
+      }
+    ]
+  }
+]);
+
+function BigSpinner() {
+  return <h2>🌀 Loading...</h2>;
+}
+
 function App() {
-  return (
-    <>
-      <Navbar />
-    </>
-  );
+  return <RouterProvider router={router} fallbackElement={<BigSpinner />} />;
 }
 
 export default App;
