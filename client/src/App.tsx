a6d6606a5299be5170621ce4697d79cfdb909199
MoosaTae <sadhis.tae@gmail.com>
2023-10-25 15:53:02 +0700

    test formatter

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,13 @@
-  import React from 'react';
-  import './App.css';
-  import Navbar from './components/Navbar';
-  import Register from '../src/components/Register';
-  import { RouterProvider} from '../'
-  function App() {
-    return (
-      <>
-        <Navbar/>
-        
-      </>
-    );
-  }
+import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import Register from '../src/components/Register';
+function App() {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+}
 
-  export default App;
+export default App;
