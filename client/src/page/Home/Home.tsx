7fe191019082c9d3f47bbbe9f3a54de80d3661fa
Your Name <you@example.com>
2023-10-27 03:07:35 +0700

    css / html for 1440 laptop only still need to fix something

--- client/src/page/Home/Home.tsx
+++ client/src/page/Home/Home.tsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import './Home.css';
 
 const Home = () => {
   return (
-    <div>This is the HomePage</div>
+    <div id = "body2">
+
+    <div id = "bg-gradient"></div>
+
+    <h1 className="text-white text-2xl font-bold" id = "headertext">สวัสดี! ,หางานอยู่หรอ?</h1>
+    <h1 className="text-white text-2xl font-bold" id = "headertext2">ให้เราช่วยคุณสิ!</h1>
+
+    <h3 className="text-white text-2xl font-bold" id = "regtext">กดเพื่อลงทะเบียนสมาชิก</h3>
+    <h3 className="text-white text-2xl font-bold" id = "logtext">หากมีบัญชีอยู่แล้ว？</h3>
+
+    <button id = "register">
+      <span>REGISTER</span>
+    </button>
+    <button  id = "login">
+      <span>LOGIN</span>
+    </button>
+
+
+    <div className="image-container">
+    <div id = "stationery_img"></div>
+    </div>
+
+    </div>
+
   )
+
 }
 
 export default Home
