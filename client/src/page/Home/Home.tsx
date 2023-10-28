ac3349775fe31e92243fb4c2f40b1c0710901c4e
Your Name <you@example.com>
2023-10-28 16:01:32 +0700

    edit z index of img container

--- client/src/page/Home/Home.tsx
+++ client/src/page/Home/Home.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 import './Home.css';
 
 const Home = () => {
@@ -12,18 +13,20 @@ const Home = () => {
 
     <h3 className="text-white text-2xl font-bold" id = "regtext">กดเพื่อลงทะเบียนสมาชิก</h3>
 
+    
+    <a href="/Register" title ="head to reg">
     <div className="btn">
-    <a href="/Register">
+
     <button id = "register"> 
       <span>
         REGISTER
       </span>
     </button>
-    </a>
+
     </div>
+    </a>
 
     <h3 className="text-white text-2xl font-bold" id = "logtext">หากมีบัญชีอยู่แล้ว？</h3>
-    
     <a href="/Login">
     <div className="btn">
     <button  id = "login">
@@ -39,15 +42,6 @@ const Home = () => {
 
     </div>
     </div>
-    <footer>
-    <div id = "footers">
-      <div id = "textfooter">
-      <h1 id = "footergetdeal">CUGETDEAL</h1>
-      <h4 id = "description">Are you a student of Chulalongkorn University, eager to jumpstart your career and find the perfect job opportunity? Look no further! CUGETDEAL is your dedicated platform to connect with exciting career prospects tailored to the unique skills and talents you've cultivated during your academic journey.</h4>
-      <h1 id = "footergetdeal2"> All Right Reserved | Designed By CUGETDEAL</h1>
-      </div>
-    </div>
-    </footer>
     </div>
 
   )
