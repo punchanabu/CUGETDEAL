d90c8c405753fb832188e793c98d1112e6507139
punpunkeshin05 <97283645+punpunkeshin05@users.noreply.github.com>
2023-10-28 23:07:35 +0700

    add indentation, some spacing and change a tag to Link tag

--- client/src/page/Home/Home.tsx
+++ client/src/page/Home/Home.tsx
@@ -1,49 +1,37 @@
-import React from 'react'
+import React from 'react';
 import { Link } from "react-router-dom";
 import './Home.css';
 
 const Home = () => {
-  return (
- 
-    <div className="w-screen h-screen p-10" id = "make-padding-buttom">
-    <div className="text-container">
-    <h1 className="text-white text-2xl font-bold" id = "headertext">สวัสดี! ,หางานอยู่หรอ?</h1>
-    <h1 className="text-white text-2xl font-bold" id = "headertext2">ให้เราช่วยคุณสิ!</h1>
+    return (
+        <div className="w-screen h-screen p-10" id="make-padding-buttom">
+            <div className="text-container space-y-4">
+                <section className='space-y-4'>
+                  <h1 className="text-white text-2xl font-bold" id="headertext">สวัสดี!, หางานอยู่หรอ?</h1>
+                  <h1 className="text-white text-2xl font-bold" id="headertext2">ให้เราช่วยคุณสิ!</h1>
+                </section>
+                <h3 className="text-white text-2xl font-bold" id="regtext">กดเพื่อลงทะเบียนสมาชิก</h3>
+                <Link to="/Register" title="head to reg">
+                    <div className="btn">
+                        <button id="register">
+                            <span>REGISTER</span>
+                        </button>
+                    </div>
+                </Link>
+                <h3 className="text-white text-2xl font-bold" id="logtext">หากมีบัญชีอยู่แล้ว？</h3>
+                <Link to="/Login">
+                    <div className="btn">
+                        <button id="login">
+                            <span>LOGIN</span>
+                        </button>
+                    </div>
+                </Link>
+            </div>
+            <div className="image-container">
+                <div id="stationery_img"></div>
+            </div>
+        </div>
+    );
+};
 
-    <h3 className="text-white text-2xl font-bold" id = "regtext">กดเพื่อลงทะเบียนสมาชิก</h3>
-
-    
-    <a href="/Register" title ="head to reg">
-    <div className="btn">
-
-    <button id = "register"> 
-      <span>
-        REGISTER
-      </span>
-    </button>
-
-    </div>
-    </a>
-
-    <h3 className="text-white text-2xl font-bold" id = "logtext">หากมีบัญชีอยู่แล้ว？</h3>
-    <a href="/Login">
-    <div className="btn">
-    <button  id = "login">
-      <span>LOGIN</span>
-    </button>
-    </div>
-    </a>
-    </div>
-
-    <div className="image-container">
-    <div id = "stationery_img"></div>
-    </div>
-
-    </div>
-
-
-  )
-
-}
-
-export default Home
+export default Home;
