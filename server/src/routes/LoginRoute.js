32303ab40c760d064724a0150445efc666c88b76
MoosaTae <sadhis.tae@gmail.com>
2023-10-29 02:58:49 +0700

    Merge branch 'main' into Homepage

---
+++ server/src/routes/LoginRoute.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import * as User from '../controllers/userController.js';
+
+const loginRouter = express.Router();
+
+loginRouter.use(express.json());
+
+// Define the registration route
+loginRouter.post('/', User.loginUser);
+
+export default loginRouter;
