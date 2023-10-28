bdac682786aba94bdddaccb7ce8108d8f431b545
Your Name <you@example.com>
2023-10-29 00:52:59 +0700

    picLoginPage

--- client/src/page/Login/Login.tsx
+++ client/src/page/Login/Login.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { verifyUser } from '../../api/UserApi';
 import { Navigate, useNavigate } from 'react-router-dom';
+import './Login.css';
 export default function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -26,27 +27,60 @@ export default function Login() {
   };
 
   return (
+
+    <div id ="bgall">
+      <div id = "p-container">   
+          <div id = "p-content">
+              <div id = "login-image">
+                  <div id = "pinkholder">
+
+                  </div>
+              </div>
+          </div>
+      <div id = "testbox">
+
+      <div id = "logo"></div>
+      <div id = "pdz"></div>
+      <div id = "sp-form">
     <form onSubmit={handleSubmit}>
-      <label>
-        Email:
+      <label id = "email_input">
         <input
           type="email"
+          placeholder="Email"
+          id = "input_border"
           value={email}
           onChange={(event) => setEmail(event.target.value)}
         />
       </label>
       <br />
-      <label>
-        Password:
+      <div id = "pdz"></div>
+      <label id =  "password_input">
+
         <input
           type="password"
+          placeholder="Password"
+          id = "input_border"
           value={password}
           onChange={(event) => setPassword(event.target.value)}
         />
       </label>
       <br />
-      <button type="submit">Submit</button>
-      
+      <div id = "pdz"></div>
+
+
+
+    <div className="btn-sub">
+      <button type= "submit" id = "subbtn">
+      <span>Submit</span>
+      </button>
+      </div>
+
+
+
     </form>
+    </div>
+      </div>
+      </div>
+    </div>
   );
 }
