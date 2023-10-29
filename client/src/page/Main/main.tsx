d80750b51430d7eb6672b9e99e4857f81fd703c6
punpunkeshin05 <97283645+punpunkeshin05@users.noreply.github.com>
2023-10-29 11:52:11 +0700

    เหลือ fetch

--- client/src/page/Main/main.tsx
+++ client/src/page/Main/main.tsx
@@ -45,7 +45,25 @@ export default function MainContent() {
             <div className = "findzone flex flex-col items-center">
                 
                 <div className = "text-white text-xl w-3/4">
-                    <p className="text-2xl">ค้นหางาน</p>
+                    <div className="flex space-x-2 items-center">
+                      <p className="text-2xl">ค้นหางาน</p>
+                      <div className="searchicon">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke-width="1.5"
+                            stroke="currentColor"
+                            className="w-6 h-6"
+                          >
+                            <path
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                              d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+                            />
+                          </svg>
+                        </div>
+                    </div>
                     <div className = "flex space-x-2">
                         <input 
                             type="textinput" 
@@ -74,4 +92,3 @@ export default function MainContent() {
         </main>
     )
 }
-
